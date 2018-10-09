87c0454fc640f0a9e43cc724972a639797f361ca
HRAND5 <asbjorn@dissing.nu>
2018-10-09 22:11:17 +0200

    changed bot.say() to bot.send_message() cause the bot responds faster with that functions

--- calculated_bot.py
+++ calculated_bot.py
@@ -19,15 +19,15 @@ def get_json(url):
     return requests.get(url).json()
 
 
-@bot.command()
-async def ping():
-    await bot.say("Pong!")
+@bot.command(pass_context=True)
+async def ping(ctx):
+    await bot.send_message(ctx.message.channel, "Pong!")
 
 
-@bot.command(name="queue", aliases="q")
-async def display_queue():
+@bot.command(name="queue", aliases="q", pass_context=True)
+async def display_queue(ctx):
     response = get_json("https://calculated.gg/api/global/queue/count")
-    await bot.say(str(response['priority 3']) + ' replays in the queue.')
+    await bot.send_message(ctx.message.channel, str(response['priority 3']) + ' replays in the queue.')
 
 
 @bot.command(name="fullqueue")
@@ -99,8 +99,8 @@ async def id(ctx):
     await bot.send_message(ctx.message.channel, "Your Calculated ID is " + responseID)
 
 
-@bot.command(name="help", aliases="h")
-async def get_help():
+@bot.command(name="help", aliases="h", pass_context=True)
+async def get_help(ctx):
     embed = discord.Embed(
         colour=discord.Colour.blue()
     )
@@ -109,7 +109,7 @@ async def get_help():
     embed.add_field(name="!help", value="Shows this message", inline=False)
     embed.add_field(name="!queue !q", value="Shows the current amount of replays in the queue.", inline=False)
 
-    await bot.say(embed=embed)
+    await bot.send_message(ctx.message.channel, embed=embed)
 
 
 @bot.event
